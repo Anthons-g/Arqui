caeb3cab225d64a980bae3f2af85a4461841e23d
Anthons-g <anthonchguerrero@gmail.com>
2025-05-22 11:11:44 -0500

    primer commit

---
+++ saludos.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+
+def saludar():
+    nombre = entradanombre.get() #se llama cuando haces click en saludar
+    if nombre.strip() == "":
+        resultado.config(text="Por favor, Pon un Nombre para saludartexd",fg= "red") # si no pusistes nombre salgra un mensaje para que pongas el nombre
+    elif any(char.isdigit() for char in nombre):
+        resultado.config(text="El nombre no debe contener números.", fg="red")
+    else:
+        resultado.config(text=f"Holaaa, {nombre}! Qué gusto, saludos :D", fg="blue") #
+        botonsaludo.config(state="disabled") #este lo que hace es desactivar el boton saludar
+        botoncerrar.pack(pady=10) #muestra el boton cerrar
+
+#crwcion de la ventana con tkinter
+ventana = tk.Tk()
+ventana.title("Ventana de saludos compas")# nombre de la ventana 
+ventana.geometry("350x200") # el tamaño con el que salgra la ventana 
+
+#aqui es donde se muestra la primer tiqueta que seria la primer etiqueta
+Etiquetapregunta = tk.Label(ventana, text="Holaaas, ¿como te llamas :)?", font=("Arial", 12))
+Etiquetapregunta.pack(pady=10)
+
+# Entrada-nombre
+entradanombre = tk.Entry(ventana, font=("Arial", 12))# comando que crea la caja de texto y vendo estara(ventana), tipo de letra
+entradanombre.pack()
+
+# Botón saludo
+botonsaludo = tk.Button(ventana, text="Saludar", font=("Arial", 11), command=saludar)
+botonsaludo.pack(pady=5)  
+
+
+# Etiqueta que sale cuando no pones nombre 
+resultado = tk.Label(ventana, text="", font=("Arial", 12))
+resultado.pack()
+
+#boton cerrar
+botoncerrar = tk.Button(ventana, text="Cerrar", font=("Arial", 11), command=ventana.destroy)
+
+#inicio mantiene la ventana abierta y si se da el destroy la cierra
+ventana.mainloop()
