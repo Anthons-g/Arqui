6598be1c4ebc4805f1e67dd8b10bb80b7a2a9142
Anthons-g <anthonchguerrero@gmail.com>
2025-05-29 11:28:05 -0500

    varible sin usar

--- saludos.py
+++ saludos.py
@@ -1,37 +1,31 @@
 import tkinter as tk
+holamund=10
 def saludar():
-    nombre = entradanombre.get() #se llama cuando haces click en saludar
+    nombre = entradanombre.get() 
     if nombre.strip() == "":
-        resultado.config(text="Por favor, Pon un Nombre para saludartexd",fg= "red") # si no pusistes nombre salgra un mensaje para que pongas el nombre
+        resultado.config(text="Por favor, Pon un Nombre para saludartexd",fg= "red") 
     elif any(char.isdigit() for char in nombre):
         resultado.config(text="El nombre no debe contener números.", fg="red")
     else:
         resultado.config(text=f"Holaaa, {nombre}! Qué gusto, saludos :D", fg="blue") #
-        botonsaludo.config(state="disable") #este lo que hace es desactivar el boton saludar
-        botoncerrar.pack(pady=10) #muestra el boton cerrar
-#crwcion de la ventana con tkinter
+        botonsaludo.config(state="disable") 
+        botoncerrar.pack(pady=10) 
 ventana = tk.Tk()
-ventana.title("Ventana de saludos compas")# nombre de la ventana 
-ventana.geometry("350x350") # el tamaño con el que salgra la ventana
+ventana.title("Ventana de saludos compas")
+ventana.geometry("350x350") 
 
-#aqui es donde se muestra la primer tiqueta que seria la primer etiqueta
 Etiquetapregunta = tk.Label(ventana, text="Holaaas, ¿como te llamas :)?", font=("Arial", 12))
 Etiquetapregunta.pack(pady=10)
 
-# Entrada-nombre
-entradanombre = tk.Entry(ventana, font=("Arial", 12))# comando que crea la caja de texto y vendo estara(ventana), tipo de letra
+entradanombre = tk.Entry(ventana, font=("Arial", 12))
 entradanombre.pack()
 
-# Botón saludo
 botonsaludo = tk.Button(ventana, text="Saludar", command=saludar)
 botonsaludo.pack(pady=5) 
 
-# Etiqueta que sale cuando no pones nombre 
 resultado = tk.Label(ventana, text="", font=("Arial", 12))
 resultado.pack()
 
-#boton cerrar
 botoncerrar = tk.Button(ventana, text="Cerrar", font=("Arial", 11), command=ventana.destroy)
 
-#inicio mantiene la ventana abierta y si se da el destroy la cierra
 ventana.mainloop()
